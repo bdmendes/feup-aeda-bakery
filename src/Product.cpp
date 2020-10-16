f7be675010de91db7f46894b4786300986d73aa3
Adelaide <lamirandasantos@gmail.com>
2020-10-16 11:51:00 +0100

    Implementei os métodos e construtores e modifiquei a estrutura dos flavours

--- src/Product.cpp
+++ src/Product.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Product.h"
+
+//CONSTRUCTORS
+
+Product::Product(const std::string &name, float price) : _name(name), _price(price) {}
+
+Bread::Bread(const std::string &name, float price, bool small) : Product(name, price), _small(small) {}
+
+Cake::Cake(const std::string &name, float price, std::string firstFlavour, std::string secondFlavour) : Product(name, price),
+        _firstFlavour(firstFlavour), _secondFlavour(secondFlavour) {
+
+}
+
+//GET METHODS
+
+std::string Product::getName() const { return _name;}
+
+float Product::getPrice() const { return _price;}
+
+bool Bread::isSmall() const { return _small;}
+
+std::string Cake::getFirstFlavour() const { return _firstFlavour;}
+
+std::string Cake::getSecondFlavour() const { return _secondFlavour;}
+
+//ADD AND REMOVE FLAVOURS
+
+void Cake::addFlavour(const std::string flavourName) { FLAVOURS.insert(flavourName);}
+
+void Cake::removeFlavour(const std::string flavourName) { FLAVOURS.erase(flavourName);}
+
+
--- src/Product.h
+++ src/Product.h
@@ -4,21 +4,19 @@
 
 #ifndef SRC_PRODUCT_H
 #define SRC_PRODUCT_H
-#include <string>
 
-enum TOPPING {
-    CRUNCHY,
-    MEAT_SAUCE,
-    SPONGE,
-    CHOCOLATE,
-    VANILLA,
-    STRAWBERRY,
-    PUFF
-};
+#include <string>
+#include <set>
+
+//if we want to add or remove flavours we cant use enum
+std::set<std::string> FLAVOURS = {"Crunchy","Meat Sauce", "Sponge","Chocolate","Vanilla","Red Velvet","Puff","Caramel","Orange",
+                                  "Apple", "Carrot","Coconut"};
 
 class Product {
 public:
     Product(const std::string &name, float price);
+    std::string getName() const;
+    float getPrice() const;
 private:
     std::string _name;
     float _price;
@@ -27,15 +25,20 @@ private:
 class Bread : public Product{
 public:
     Bread(const std::string &name, float price, bool small);
+    bool isSmall() const;
 private:
     float _small;
 };
 
 class Cake : public Product{
 public:
-    Cake(const std::string &name, float price, enum TOPPING firstTopping, enum TOPPING secondTopping);
+    Cake(const std::string &name, float price, std::string firstFlavour, std::string secondFlavour);
+    std::string getFirstFlavour() const;
+    std::string getSecondFlavour() const;
+    void addFlavour(const std::string flavourName);
+    void removeFlavour(const std::string flavourName);
 private:
-    enum TOPPING _firstTopping;
-    enum TOPPING _secondTopping;
+    std::string _firstFlavour;
+    std::string _secondFlavour;
 };
 #endif //SRC_PRODUCT_H
